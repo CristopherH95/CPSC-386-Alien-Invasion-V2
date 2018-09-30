e35c923c2a08e24120eb20fabd4c03bd45a57471
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-09-30 12:30:25 -0700

    Convert high score storage to list format, for additional scores

--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,7 @@ class GameStats:
         self.ai_settings = ai_settings
         self.ships_left = 0
         self.high_score = None
+        self.high_scores_all = None
         self.score = None
         self.level = None
         self.reset_stats()
@@ -23,12 +24,19 @@ class GameStats:
         """Read the saved high score from the json file on disk (if it exists)"""
         try:
             with open('score_data.json', 'r') as file:
-                self.high_score = int(json.load(file))    # Cast to int to verify type
-        except (FileNotFoundError, ValueError, EOFError, json.JSONDecodeError) as e:
+                self.high_scores_all = json.load(file)  # Cast to int to verify type
+                self.high_scores_all.sort(reverse=True)
+                self.high_score = self.high_scores_all[0]
+        except (FileNotFoundError, ValueError, EOFError, json.JSONDecodeError, AttributeError, IndexError) as e:
             print(e)
-            self.high_score = 0     # Some issue with the file, going to default
+            self.high_scores_all = [0, 0, 0]    # Some issue with the file, going to default
+            self.high_score = self.high_scores_all[0]
 
     def save_high_score(self):
         """Save the high score to a json file on disk"""
+        for i in range(len(self.high_scores_all)):
+            if self.score >= self.high_scores_all[i]:
+                self.high_scores_all[i] = self.score
+                break
         with open('score_data.json', 'w') as file:
-            json.dump(self.high_score, file)
+            json.dump(self.high_scores_all, file)
